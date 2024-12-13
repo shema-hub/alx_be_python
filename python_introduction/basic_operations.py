6f306179a2f0d9991de3debebb7fc35c3b6f1c59
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2024-12-13 17:37:29 +0000

    repairing codes

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,13 @@
 # Define the numbers
-number1 = int(10)
-number2 = int(5)
+num1 = 10
+num2 = 5
 
 # Perform operations
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
 
 # Print results
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+print(f"Addition of {num1} and {num2} is {addition}")
+print(f"Subtraction of {num1} and {num2} is {subtraction}")
+print(f"Multiplication of {num1} and {num2} is {multiplication}")
